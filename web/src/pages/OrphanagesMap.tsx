49499c154cd71caa3054f9a200feccf0f765ac02
Diosefer Bueno <diosefer.ti@gmail.com>
2020-12-25 14:50:57 -0400

    foi inserida a busca da localidade pelo mapa da google, correções de bugs anteriores e melhoria de código

--- web/src/pages/OrphanagesMap.tsx
+++ web/src/pages/OrphanagesMap.tsx
@@ -1,18 +1,37 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react'; 
+//#region   a importação useEffect é uma função onde eu passo no primeiro parametro o que 
+//eu quero fazer e no segundo quando eu quero que faça o primeiro {} será o que deve ser feito,
+//o segundo[] 
+//#endregion será quando uma determinada variável que eu colocar dentro do vetor mudar
 
 import mapMarkerImg from '../images/map-marker.svg';
 import {Link} from 'react-router-dom';
-import {FiPlus} from 'react-icons/fi';
+import {FiPlus, FiArrowRight} from 'react-icons/fi';
 
 import'../styles/pages/orphanages-map.css';
 
-import {Map, TileLayer} from 'react-leaflet';
-import { tileLayer } from 'leaflet';
+import {Map, TileLayer, Marker, Popup} from 'react-leaflet';
 
-import 'leaflet/dist/leaflet.css';
+import mapIcon from '../utils/mapIcon';
+import api from '../services/api';
+
+interface Orphanage{
+    id: number;
+    latitude: number;
+    longitude: number;
+    name: string;
+}
 
 function OrphanagesMap()
 {
+    const [orphanages,setOrphanages] = useState<Orphanage[]>([]);
+
+    useEffect(() => {
+        api.get("orphanages").then(response =>{
+             setOrphanages(response.data);
+        });
+    }, [])
+
     return (
         <div id="page-map">
             <aside>
@@ -36,15 +55,30 @@ function OrphanagesMap()
             >
                 <TileLayer url="https://a.tile.openstreetmap.org/{z}/{x}/{y}.png"/>
                 
-
-                
+               {orphanages.map(orphanage => {
+                   return (
+                        <Marker
+                        icon = {mapIcon}
+                        position = {[orphanage.latitude,orphanage.longitude]}
+                        key={orphanage.id}
+                        >
+                            <Popup closeButton={false} minWidth={240} maxWidth={240} className="map-popup">
+                                {orphanage.name}
+                                <Link to={'/orphanages/'+ orphanage.id}>
+                                <FiArrowRight size={20} color= "#FFF"/>
+                                </Link>
+                            </Popup>
+                        </Marker>
+                   )
+               })}                
                 {/* <TileLayer 
                 url={'https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/256/{z}/{x}/{y}@2x?access_token=${process.env.REACT_APP_MAPBOX_TOKEN}}'} 
                 /> */}
 
+
                 </Map>
 
-            <Link to="" className="create-orphanage">
+            <Link to="/orphanages/create" className="create-orphanage">
                 <FiPlus size={32} color="#FFF" />
             </Link>
         </div>
